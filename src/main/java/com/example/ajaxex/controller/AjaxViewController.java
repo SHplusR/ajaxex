5b76fe0a7a802656658a5a7bb637f0c0eeeaf20a
SHplusR <111962797+SHplusR@users.noreply.github.com>
2023-06-15 12:10:50 +0900

    ajax 완료

--- src/main/java/com/example/ajaxex/controller/AjaxViewController.java
+++ src/main/java/com/example/ajaxex/controller/AjaxViewController.java
@@ -1,2 +1,56 @@
-package com.example.ajaxex.controller;public class AjaxViewController {
+package com.example.ajaxex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AjaxViewController {
+    @GetMapping("/ajax-ex-01")
+    public String ajaxEx01(){
+        return "ajax-ex-01.html";
+    }
+
+    @GetMapping("/ajax-ex-02")
+    public String ajaxEx02(){
+        return "ajax-ex-02.html";
+    }
+    @GetMapping("/ajax-ex-03")
+    public String ajaxEx03(){
+        return "ajax-ex-03.html";
+    }
+
+    @GetMapping("/ajax-ex-04")
+    public String ajaxEx04(){
+        return "ajax-ex-04.html";
+    }
+
+    @GetMapping("/ajax-ex-05")
+    public String ajaxEx05(){
+        return "ajax-ex-05.html";
+    }
+
+    @GetMapping("/ajax-ex-06")
+    public String ajaxEx06(){
+        return "ajax-ex-06.html";
+    }
+
+    @GetMapping("/ajax-ex-07")
+    public String ajaxEx07(){
+        return "ajax-ex-07.html";
+    }
+
+    @GetMapping("/ajax-ex-08")
+    public String ajaxEx08(){
+        return "ajax-ex-08.html";
+    }
+
+    @GetMapping("/ajax-ex-09")
+    public String ajaxEx09(){
+        return "ajax-ex-09.html";
+    }
+
+    @GetMapping("/ajax-ex-10")
+    public String ajaxEx10(){
+        return "ajax-ex-10.html";
+    }
 }
--- src/main/java/com/example/ajaxex/controller/HomeController.java
+++ src/main/java/com/example/ajaxex/controller/HomeController.java
@@ -1,2 +1,15 @@
-package com.example.ajaxex.controller;public class HomeController {
+package com.example.ajaxex.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
 }
